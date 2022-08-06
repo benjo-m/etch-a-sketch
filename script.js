678d2661efa58fa6b6df067584071db8f0755d86
benjo-m <maglicbenjamin@gmail.com>
2022-08-06 17:31:55 +0200

    Added rainbow mode

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 const changeSizeBtn = document.getElementById("btnChange");
 let colorInput = document.getElementById("coloring");
-let eraser = document.getElementById("eraser");
 const defaultSize = 16;
 
+function makeContainer(gridSize) {
+    let container = document.getElementById("container");
+    let eraser = document.getElementById("eraser");
+    let rainbow = document.getElementById("rainbow");
+
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        let square = document.createElement("div");
+        square.addEventListener("mouseover", () => {
+            if (eraser.checked) {
+                square.style.backgroundColor = "#fff";
+            } else {
+                if (rainbow.checked) {
+                    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+                    square.style.backgroundColor = `#${randomColor}`;
+                } else {
+                    square.style.backgroundColor = `${colorInput.value}`
+                }
+            }
+        })
+    container.appendChild(square)
+    }
+}
 
 function changeSize() {
     let newSize = document.getElementById("gridSize").value;
     document.getElementById("container").innerHTML = "";
     makeContainer(newSize);
-}
-
-function makeContainer(gridSize) {
-    let container = document.getElementById("container");
-    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-    for (let i = 0; i < gridSize * gridSize; i++) {
-        let square = document.createElement("div");
-        square.addEventListener("mouseover", () => {
-            eraser.checked ? square.style.backgroundColor = "#fff" 
-            : square.style.backgroundColor = `${colorInput.value}`
-        })
-    container.appendChild(square)
-    }
 }
 
 makeContainer(defaultSize);
